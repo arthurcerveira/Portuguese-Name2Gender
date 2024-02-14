3468f0453ccd1045fefb1feba10380d654487908
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2024-02-14 14:49:46 -0300

    Add requirements and example script

---
+++ example.py
@@ -0,0 +1,8 @@
+from pipeline import name_to_gender_pipeline
+
+# "Adrevaldo" and "Devandra" are not in the dataset and should be classified as M and F
+names = ["João", "Maria", "Adrevaldo", "Devandra"]
+
+for name in names:
+    gender = name_to_gender_pipeline(name)
+    print(name, gender)
